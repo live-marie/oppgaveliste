785446d990e2e368d0a589681c1b1ab446eec89c
Live Marie Sørsdahl <live@snipp.no>
2021-01-13 14:46:13 +0100

    laget liste til oppgaver

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 
 
-antall_oppgaver = input("hvor mange oppgaver skal listen ha? ")
+antall_oppgaver = int( input("hvor mange oppgaver skal listen ha? "))
 print(antall_oppgaver)
+
+oppgaver = []
+for i in range (antall_oppgaver):
+    oppgave_navn = input("hva skal oppgaven hete? ")
+    oppgaver.append (oppgave_navn)
+print(oppgaver)
+
+
+
