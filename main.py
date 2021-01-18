38767a71dfcca18d5e283bf7fcbeae1a24b2a997
Live Marie Sørsdahl <live@snipp.no>
2021-01-18 18:08:21 +0100

    vi har laget en fil

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ for i in range (antall_oppgaver):
     oppgave_navn = input("hva skal oppgaven hete? ")
     oppgaver.append (oppgave_navn)
 print(oppgaver)
+fil = open("oppgaver.txt", "a")
+for oppgave in oppgaver:
+    fil.write(oppgave)
+fil.close()
 
 
 
