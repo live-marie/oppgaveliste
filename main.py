d9d63c1bfcced040f4d779eaf011cc238baaf4f4
Live Marie Sørsdahl <live@snipp.no>
2021-03-25 17:44:37 +0100

    har laget klasse og objekter

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 
 
+class Oppgave:
+    def __init__(self, navn):
+        self.navn = navn
+        self.gjort = False
+
 antall_oppgaver = int( input("hvor mange oppgaver skal listen ha? "))
 print(antall_oppgaver)
 
 oppgaver = []
 for i in range (antall_oppgaver):
     oppgave_navn = input("hva skal oppgaven hete? ")
-    oppgaver.append (oppgave_navn)
-print(oppgaver)
+    oppgave = Oppgave(oppgave_navn)
+    oppgaver.append (oppgave)
+
+for o in oppgaver:
+    print(o.navn)
 
 def skriv_til_fil(filnavn):
     fil = open(filnavn, "a")
     for oppgave in oppgaver:
-        fil.write(oppgave)
+        fil.write(oppgave.navn + "\n")
     fil.close()
 
 skriv_til_fil("oppgaver til liste.txt")
+
 
 
